a8894b990b15f45940c0946c72a2b1dafd9fcb22
Belle <bstorm@sandia.gov>
2026-04-09 11:23:11 -0600

    add fixtures

--- gtep/tests/unit/test_model_library/test_scaling.py
+++ gtep/tests/unit/test_model_library/test_scaling.py
@@ -15,3 +15,32 @@
 Unit tests for the scaling function for Generation and Transmission Expansion Planning (GTEP)
 Model
 """
+
+import pytest
+from os.path import abspath, join, dirname
+from gtep.model_library.scaling import add_load_scaling
+from gtep.gtep_model import ExpansionPlanningModel, create_stages
+from gtep.gtep_data import ExpansionPlanningData
+
+
+@pytest.fixture
+def create_test_model():
+    # create the ConcreteModel
+    curr_dir = dirname(abspath(__file__))
+    debug_data_path = abspath(join(curr_dir, "..", "..", "data", "5bus"))
+    dataObject = ExpansionPlanningData()
+    dataObject.load_prescient(debug_data_path)
+    modObject = ExpansionPlanningModel(data=dataObject)
+    m = modObject.create_model()
+    return modObject
+
+
+# m is the model
+# b is the representative period
+@pytest.fixture
+def create_rep(m):
+    pass
+
+
+def test_add_load_scaling():
+    pass
